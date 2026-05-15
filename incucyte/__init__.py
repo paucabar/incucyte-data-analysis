372d29575902b6b746e64dbb1833bad3c4af97cb
unknown <pau.carrillob@gmail.com>
2026-05-15 15:24:44 +0200

    operetta txt loader

--- incucyte/__init__.py
+++ incucyte/__init__.py
@@ -1,4 +1,4 @@
-from .load import load_qupath, load_cellprofiler, load_plate_map, merge_plate_map
+from .load import load_qupath, load_cellprofiler, load_operetta, load_plate_map, merge_plate_map
 from .filter import filter_by, filter_not_null, filter_timepoints
 from .classify import classify_threshold
 from .aggregate import to_well_means, to_replicate_means, class_proportions
@@ -6,7 +6,7 @@ from .plot import time_series, boxplot, paired_boxplot, qc_counts, metric_heatma
 from .plate import well_sort_key
 
 __all__ = [
-    "load_qupath", "load_cellprofiler", "load_plate_map", "merge_plate_map",
+    "load_qupath", "load_cellprofiler", "load_operetta", "load_plate_map", "merge_plate_map",
     "filter_by", "filter_not_null", "filter_timepoints",
     "classify_threshold",
     "to_well_means", "to_replicate_means", "class_proportions",
